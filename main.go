d97e74c91a411bd0f561b7ed36965feec7989f34
Papi Diagne <abdiagne96@gmail.com>
2023-04-24 23:44:36 +0000

    improve code style

--- main.go
+++ main.go
@@ -7,22 +7,26 @@ import (
 
 func showMenu(choices []string) {
 	fmt.Println("Enter the number corresponding to your choice")
+
 	for i := 0; i < len(choices); i++ {
 		fmt.Println(i+1, "->", choices[i])
 	}
+
 	var userInput string
+
 	fmt.Scanln(&userInput)
 	userChoice, err := strconv.Atoi(userInput)
-
 	if err != nil {
 		fmt.Println("Invalid input. Please retry!")
 		showMenu(choices)
+
 		return
 	}
 
 	if userChoice < 1 || userChoice > len(choices) {
 		fmt.Println("Choice unavailable. Please retry!")
 		showMenu(choices)
+
 		return
 	}
 
