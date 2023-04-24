97fafd7d175db4c4765e5d40b61e6ba1a81376f9
Papi Diagne <abdiagne96@gmail.com>
2023-04-24 23:05:08 +0000

    fix variable names

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "strconv"
+	"fmt"
+	"strconv"
 )
 
+func showMenu(choices []string) {
+	fmt.Println("Enter the number corresponding to your choice")
+	for i := 0; i < len(choices); i++ {
+		fmt.Println(i+1, "->", choices[i])
+	}
+	var userInput string
+	fmt.Scanln(&userInput)
+	userChoice, err := strconv.Atoi(userInput)
 
-func show_menu(choices []string) {
-  fmt.Println("Enter the number corresponding to your choice")
-  for i:=0; i<len(choices); i++ {
-    fmt.Println(i+1,"->",choices[i])
-  }
-  var user_input string
-  fmt.Scanln(&user_input)
-  user_choice, err := strconv.Atoi(user_input)
+	if err != nil {
+		fmt.Println("Invalid input. Please retry!")
+		showMenu(choices)
+		return
+	}
 
-  if err != nil {
-    fmt.Println("Invalid input. Please retry!")
-    show_menu(choices)
-    return
-  }
+	if userChoice < 1 || userChoice > len(choices) {
+		fmt.Println("Choice unavailable. Please retry!")
+		showMenu(choices)
+		return
+	}
 
-  if user_choice<1 || user_choice>len(choices) {
-    fmt.Println("Choice unavailable. Please retry!")
-    show_menu(choices)
-    return
-  }
-
-  fmt.Println(user_choice)
-  fmt.Println("You chose to", choices[user_choice-1])
+	fmt.Println(userChoice)
+	fmt.Println("You chose to", choices[userChoice-1])
 }
 
-
 func main() {
-  fmt.Println("Welcome to the monty hall game!")
-  show_menu([]string{"Play", "Quit"})
+	fmt.Println("Welcome to the monty hall game!")
+	showMenu([]string{"Play", "Quit"})
 }
